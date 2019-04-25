36bf00b14542cac0cb7cd908cb8a903f997e258d
webtoor <fityanalimunshi@gmail.com>
2019-04-25 15:51:36 +0700

    generate TabsPage + Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' }
 ];
 
 @NgModule({
